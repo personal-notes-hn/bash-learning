b29e95c0a725b82cba26d817d10535defc9e39c3
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-05-15 22:58:13 +0700

    learning variables

--- BestDayEver.sh
+++ BestDayEver.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Good morning, Tinker!"
+# echo "What is your name?"
+# read name
+
+name=$1
+
+echo "Good morning, $name!"
 sleep 1
-echo "You're looking good today Tinker!"
+echo "You're looking good today $name!"
 sleep 1
-echo "You have the best beard I've ever seen Tinker!"
+echo "You have the best beard I've ever seen $name!"
