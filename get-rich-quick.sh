b8017898b033c262ae631cb5524d2d99551b28c4
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-05-16 23:48:46 +0700

    finished lesson 3

--- get-rich-quick.sh
+++ get-rich-quick.sh
@@ -7,3 +7,7 @@ echo "How old are you?"
 read age
 
 echo "Hello, $name, you are $age years old."
+sleep 2
+
+years=$(( ($RANDOM % 15) + $age ))
+echo "$name, you will become a millionaire when you are $years years old."
