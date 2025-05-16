5e80bb9ae310db9696422f25383bf9ed9f561b11
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-05-16 15:19:10 +0700

    finished lesson 2

--- bestDayEver.sh
+++ bestDayEver.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-name="Techies"
+# name="Tinker"
 
 # echo "What is your name?"
 # read name
 
-# name=$1
+name=$1
+compliment=$2
+
+user=$(whoami)
+date=$(date)
+location=$(pwd)
 
 echo "Good morning, $name!"
 sleep 1
 echo "You're looking good today $name!"
 sleep 1
-echo "You have the best beard I've ever seen $name!"
+echo "You have the best $compliment I've ever seen $name!"
+sleep 1
+echo "You are currently logged in as $user and you are in the directory $location."
+echo "Also, today is: $date."
