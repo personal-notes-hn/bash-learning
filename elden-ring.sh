297fe85abaa1ffe323ac3ab335f55da0e3133289
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-05-17 23:41:34 +0700

    finished second round with the boss

--- elden-ring.sh
+++ elden-ring.sh
@@ -13,5 +13,19 @@ if [[ $human == $beast ]]; then
 	echo "Beast vanquished! Congrats fellow tarnished!"
 else
 	echo "You died."
+	exit 1
 fi
 
+sleep 2
+
+echo "Boss battle. Get scared. It's Margit. Pick a number between 0-9:"
+read human
+
+beast=$(( $RANDOM % 10 ))
+echo "Beast number is: $beast"
+
+if [[ $beast == $human ]]; then
+	echo "You win!"
+else
+	echo "You lose!"
+fi
