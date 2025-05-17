53948b8eaa039c97aaa66a932794b69e806bff79
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-05-17 18:01:39 +0700

    learning conditional bash syntax

--- elden-ring.sh
+++ elden-ring.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "You Died"
+
+echo "Hey, do you like coffee? (y/n)"
+read answer
+
+if [[ $answer == "y" ]]; then
+	echo "You are awesome!"
+else
+	echo "Leave right now!!"
+fi
+
