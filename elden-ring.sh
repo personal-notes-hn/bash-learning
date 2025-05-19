615da8798522d2b21a813576359d6e75563bb534
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-05-19 19:40:47 +0700

    finished nested IF / ELIF / CASE statement

--- elden-ring.sh
+++ elden-ring.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-echo "You died"
+echo "Welcome tarnished. Please select you starting class:
+1 - Samurai
+2 - Prisoner
+3 - Prophet"
+
+read class
+
+case $class in
+	1)
+		type="Samurai"
+		hp=10
+		attack=20
+		;;
+	2)
+		type="Prisoner"
+		hp=20
+		attack=4
+		;;
+	3)
+		type="Prophet"
+		hp=30
+		attack=4
+		;;
+esac
+
+echo "You chose the $type class. Your HP is $hp and you attack is $attack."
 
 # First beast battle
 echo "Choose your number? (0/1)"
